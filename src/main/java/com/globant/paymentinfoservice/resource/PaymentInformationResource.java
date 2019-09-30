acc5a5de111afe6b3bb687fc4df49bb1694b1d80
Daniel <dvago1988@gmail.com>
2019-09-30 00:25:17 -0500

    Minor endpoint fix

--- src/main/java/com/globant/paymentinfoservice/resource/PaymentInformationResource.java
+++ src/main/java/com/globant/paymentinfoservice/resource/PaymentInformationResource.java
@@ -6,9 +6,11 @@ import com.globant.paymentinfoservice.service.PaymentService;
 import com.globant.paymentinfoservice.service.UserLessonsService;
 import com.globant.paymentinfoservice.service.UserLessonsServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,14 +28,18 @@ public class PaymentInformationResource {
     this.paymentService = paymentService;
   }
 
-  @PostMapping("/pay")
-  public UserLessons createPayment(PaymentInformation paymentInformation) {
+  @PostMapping(value = "/",
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public UserLessons createPayment(@RequestBody PaymentInformation paymentInformation) {
     //Process payment, this should be done in a different service...
     paymentService.save(paymentInformation);
 
     //Call service to update number of total classes. If fails it should rollback the  payment transaction
     UserLessons userLessons = userLessonsService.findByUserId(paymentInformation.getUserId());
-    userLessons.addLessons(paymentInformation.getNumberOfLessons());
+    if (userLessons == null) {
+      userLessons = new UserLessons(paymentInformation.getUserId(), paymentInformation.getNumberOfLessons());
+    }
     userLessons = userLessonsService.save(userLessons);
 
     return userLessons;
